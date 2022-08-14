fce8125d1b7be3af28421cd93bd7162a16e496f2
afaqalii <96257586+afaqalii@users.noreply.github.com>
2022-08-14 12:52:55 +0500

    laram advane shuam

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const OpenMenu = document.querySelector('.nav-link')
 Humburger.addEventListener('click',()=> {
      OpenMenu.classList.toggle('active')
 })
+
+let tl = gsap.timeline({defaults:{ease:'power2.out'}})
+
+tl.from('.landing-img', {xPercent:150})
+tl.from('.landing-content', {yPercent:150})
+tl.from('nav', {opacity:0})
+tl.from('.features', {yPercent:150})
