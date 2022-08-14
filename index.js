e432151c70101edcaa5506a1fb874b5767ae378f
afaqalii <96257586+afaqalii@users.noreply.github.com>
2022-08-14 18:16:59 +0500

    ok done

--- index.js
+++ index.js
@@ -4,6 +4,56 @@ const OpenMenu = document.querySelector('.nav-link')
 Humburger.addEventListener('click',()=> {
      OpenMenu.classList.toggle('active')
 })
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.from('.features,.features-items',{
+     scrollTrigger: {
+          trigger:'.features,.features-items',
+          start:"-200% bottom",
+          toggleActions:"restart",
+     },
+     xPercent:-150
+})
+gsap.from('.features-detail',{
+     scrollTrigger: {
+         trigger:".features-detail",
+         start:'5% bottom',
+         toggleActions:'restart'        
+     },
+     xPercent:-150
+})
+gsap.from('.downlaod-section-content',{
+     scrollTrigger: {
+         trigger:".downlaod-section-content",
+         start:'5% bottom',
+         toggleActions:'restart'        
+     },
+     xPercent:-150
+})
+gsap.from('.download-section-cards',{
+     scrollTrigger: {
+         trigger:".download-section-cards",
+         start:'5% bottom',
+         toggleActions:'restart'        
+     },
+     xPercent:-150
+})
+gsap.from('.faqs',{
+     scrollTrigger: {
+         trigger:".faqs",
+         start:'5% bottom',
+         toggleActions:'restart'        
+     },
+     xPercent:-150
+})
+gsap.from('.faqs-content',{
+     scrollTrigger: {
+         trigger:".faqs-content",
+         start:'5% bottom',
+         toggleActions:'restart'        
+     },
+     xPercent:-150
+})
 
 let tl = gsap.timeline({defaults:{ease:'power2.out'}})
 
